310cdfe2bf415dd300c7b7c68b4ef6a80f81d896
julybarros <julianajulyy22@gmail.com>
2023-03-29 09:37:31 -0300

    livros

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,48 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Cadastro } from "./pages/Cadastro/Cadastro";
-import { Login } from "./pages/Login/Login";
 import { Home } from "./pages/Home/Home";
+import { Login } from "./pages/Login/Login";
 import { Root } from "./pages/Root/Root";
 import { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase/config";
+import { AuthContext } from "./contexts/AuthContext";
+import { AdicionarLivro } from "./pages/AdicionarLivro/AdicionarLivro";
+import { Livros } from "./pages/Livros/Livros";
 
 export function App() {
-    return (
-        <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Root />}>
-                        <Route path="/" element={<Home />} />
-                    </Route>
-                    <Route path="/login" element={< Login />} />
-                    <Route path="/cadastro" element={< Cadastro />} />
-                </Routes>
-            </BrowserRouter>
-            <Toaster />
-        </>
-    );
+  const [usuarioLogado, setUsuarioLogado] = useState(null);
+
+  useEffect(() => {
+    // Monitorar/detectar o usuário conectado
+    // Fica sabendo quando loga/desloga
+    onAuthStateChanged(auth, (user) => {
+      // user é nulo = deslogado
+      // user tem objeto = logado
+      setUsuarioLogado(user);
+    });
+
+    // Esse efeito irá rodar apenas uma vez
+    // Quando o App for renderizado/inicializado
+  }, []);
+
+  return (
+    <>
+      <AuthContext.Provider value={usuarioLogado}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Root />}>
+              <Route path="/" element={<Home />} />
+              <Route path="/livros" element={<Livros />} />
+              <Route path="/livros/adicionar" element={<AdicionarLivro />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/cadastro" element={<Cadastro />} />
+          </Routes>
+        </BrowserRouter>
+      </AuthContext.Provider>
+      <Toaster />
+    </>
+  );
 }
