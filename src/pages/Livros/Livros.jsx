f7ab706fce097a0d5c80ca1cea19cadfdd6e0092
julybarros <julianajulyy22@gmail.com>
2023-03-29 21:34:59 -0300

    emprestimos

--- src/pages/Livros/Livros.jsx
+++ src/pages/Livros/Livros.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react";
 import { Button, Container, Table } from "react-bootstrap";
+import { toast } from "react-hot-toast";
 import { Link } from "react-router-dom";
 import { Loader } from "../../components/Loader/Loader";
-import { getLivros } from "../../firebase/livros";
+import { deleteLivro, getLivros } from "../../firebase/livros";
 import "./Livros.css";
 
 export function Livros() {
@@ -10,10 +11,24 @@ export function Livros() {
     const [livros, setLivros] = useState(null);
 
     useEffect(() => {
-        getLivros().then(busca => {
-            setLivros(busca) 
-        })
+        initializeTable();
     }, []);
+
+    function initializeTable() {
+        getLivros().then(resultados => {
+            setLivros(resultados)
+        })
+    }
+
+    function onDeleteLivro(id, titulo) {
+        const deletar = window.confirm(`Tem certeza que deseja excluir o livro ${titulo}?`);
+        if(deletar) {
+            deleteLivro(id).then(() => {
+                toast.success(`${titulo} apagado com sucesso!`, {duration: 2000, position: "bottom-right"});
+                initializeTable();
+            })
+        }
+    }
 
     return (
         <div className="livros">
@@ -36,6 +51,7 @@ export function Livros() {
                                 <th>Categoria</th>
                                 <th>ISBN</th>
                                 <th>Imagem</th>
+                                <th>Ações</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -48,6 +64,20 @@ export function Livros() {
                                         <td>{livro.isbn}</td>
                                         <td>
                                             <img src={livro.urlCapa} alt={livro.titulo} />
+                                        </td>
+                                        <td>
+                                            <Button
+                                                as={Link}
+                                                to={`/livros/editar/${livro.id}`}
+                                                variant="warning"
+                                                size="sm"
+                                                className="me-2"
+                                            >
+                                                <i className="bi bi-pencil-fill"></i>
+                                            </Button>
+                                            <Button size="sm" variant="danger" onClick={() => onDeleteLivro(livro.id, livro.titulo)}>
+                                                <i className="bi bi-trash3-fill"></i>
+                                            </Button>
                                         </td>
                                     </tr>
                                 )
